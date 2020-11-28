2753449827e194a1867e8db46eb80b6c35963a56
vladsakun <vlad120403@gmail.com>
2020-11-28 23:09:17 +0200

    remove edge-to-edge SplashActivity.kt

--- app/src/main/java/com/supter/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/supter/ui/splash/SplashActivity.kt
@@ -15,15 +15,6 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        window.decorView.systemUiVisibility =
-                // Tells the system that the window wishes the content to
-                // be laid out at the most extreme scenario. See the docs for
-                // more information on the specifics
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    // Tells the system that the window wishes the content to
-                    // be laid out as if the navigation bar was hidden
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-
         if (SystemUtils.getToken(applicationContext).isNotEmpty()) {
             startMainActivity()
         } else {
